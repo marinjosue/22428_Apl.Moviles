1ac444c64b4462d75902d71879c39f71d495731a
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-08-03 23:53:48 -0500

    update backen

--- Proyecto/backend/app/chat.py
+++ Proyecto/backend/app/chat.py
@@ -5,7 +5,7 @@ import os
 
 GOOGLE_API_KEY = "AIzaSyCOZuAp-t-l9IIDRNZwdujv1CbIlIshnTA"
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-2.5-flash') 
+model = genai.GenerativeModel('gemini-1.5-flash') 
 
 router = APIRouter()
 
